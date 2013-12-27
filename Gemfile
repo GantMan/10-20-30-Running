a9464851744570438110c66533666c8e16d847df
Gant <GantMan@gmail.com>
2013-12-26 21:37:54 -0600

    Awesome digital counter added

--- Gemfile
+++ Gemfile
@@ -3,6 +3,6 @@ source "https://rubygems.org"
 gem "ProMotion", "~> 1.1.0"
 # gem "teacup" # advanced styling
 # gem "bubble-wrap" # lots of goodies
-gem "sugarcube" # monkeypatch all the things
+gem "sugarcube", :require => 'sugarcube-common' # monkeypatch things
 # gem "rmq" # jQuery for RubyMotion
 gem 'motion-cocoapods'
--- app/screens/home_screen.rb
+++ app/screens/home_screen.rb
@@ -6,6 +6,10 @@ class HomeScreen < PM::Screen
   def on_load
     set_nav_bar_button :left, title: "Help", action: :help_tapped
     set_nav_bar_button :right, title: "Settings", action: :states_tapped
+    # TODO: This is blowing up the # of views, with no deletion.
+    1.second.every do
+      draw_seconds(Time.now.sec)
+    end
   end
 
   def will_appear
@@ -16,6 +20,7 @@ class HomeScreen < PM::Screen
     set_attributes self.view, :home_view_style 
     title = add FBBitmapFontView.new, :title_label_style
     text_label = add UILabel.new, :label_view_style
+    $myview = self
     true
   end
 
@@ -29,4 +34,23 @@ class HomeScreen < PM::Screen
     open_modal HelpScreen.new(nav_bar: true)
   end
 
+  def draw_seconds message
+    frame = CGRectMake(60, 300, 300, 50)
+    v = FBLCDFontView.alloc.initWithFrame(frame)
+    v.text = "%02d" % message # force leading zeros
+    v.lineWidth = 8
+    v.drawOffLine = true
+    v.edgeLength = 40
+    v.margin = 10
+    v.backgroundColor = :black.uicolor
+    v.horizontalPadding = 30
+    v.verticalPadding = 14
+    v.glowSize = 10.0
+    v.glowColor = 0x00ffff.uicolor
+    v.innerGlowColor = 0x00ffff.uicolor
+    v.innerGlowSize = 3.0
+    self.view.addSubview(v)
+    v.resetSize
+    return v
+  end
 end
