4175f64be0fa77aa78e6cef315e57c514148a37d
Gant <GantMan@gmail.com>
2014-01-02 15:27:06 -0600

    Settings page pretty usable in formotion

--- app/screens/help_screen.rb
+++ app/screens/help_screen.rb
@@ -16,36 +16,46 @@ class HelpScreen < PM::FormotionScreen
   def table_data
     {
       sections: [{
-        title: "Cycles",
+        title: "Exercise Plan",
         rows: [{
-          title: "Total Cycles",
-          key: :email,
-          placeholder: "example@kohactive.com",
-          type: :string,
-          auto_correction: :no,
-          auto_capitalization: :none
-        },
-        {
-          title: "Password",
-          key: :password,
-          placeholder: "",
-          type: :string,
-          auto_correction: :no,
-          auto_capitalization: :none
+          title: "Total Cycle Blocks",
+          key: :cycles_count,
+          type: :picker,
+          items: [['Forever', '0'], ['1','1'], ['2','2'], ['3','3'], ['4','4'], ['5','5']],
+          input_accessory: :done
+        },{
+          title: "Seconds Between Blocks",
+          key: :rest_blocks,
+          type: :number,
+          input_accessory: :done
         }]
-      },
-      {
+      },{
         title: "Sound",
         rows: [{
-          title: "Alert Sound",
+          title: "Alert Sounds",
           key: :alert_sound,
-          type: :string
-
-        },
-        {
+          type: :subform,
+          display_key: :sound,
+          subform: {
+            title: "Alert Sounds",
+            sections: [{
+              key: :sound,
+              select_one: true,
+              rows: [{
+                title: "Beeps",
+                key: :Beeps,
+                type: :check
+              },{
+                title: "Boops",
+                key: :Boops,
+                type: :check
+              }]
+            }]
+          }
+        },{
           title: "Lead Up Alert",
           key: :lead_up_alert,
-          type: :string
+          type: :switch
         }]
       }]
     }
@@ -56,34 +66,3 @@ class HelpScreen < PM::FormotionScreen
   end
 end
 
-# class HelpScreen < PM::GroupedTableScreen
-#   title "Help"
-  
-
-#   def table_data
-#     [
-#       {
-#       title: "Cycles",
-#       cells: [
-#         { title: "Total Cycles", action: :email_us }
-#       ]
-#       },
-#       {
-#       title: "Sound",
-#       cells: [
-#         { title: "Alert Sound", action: :email_us },
-#         {
-#           title: "Lead Up Alert",
-#           accessory: {
-#             view: :switch,
-#             action: :lead_up_switch,
-#             arguments: { message: "I'm switched!" }
-#           }
-#         }
-#       ]
-#       }
-
-#     ]
-#   end
-
-# end
